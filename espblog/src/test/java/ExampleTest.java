93c9ad2d47ab1a1495fcb8307a162e98eb85afcf
Jocelyn Batton <jocelyn.batton@valtech.fr>
2018-11-15 11:53:35 +0100

    upgrading sling dependencies for testing

--- espblog/src/test/java/ExampleTest.java
+++ espblog/src/test/java/ExampleTest.java
@@ -1,2 +1,19 @@
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.testing.mock.sling.ResourceResolverType;
+import org.apache.sling.testing.mock.sling.junit.SlingContext;
+import org.junit.Rule;
+import org.junit.Test;
+
 public class ExampleTest {
+
+    @Rule
+    public final SlingContext context = new SlingContext(ResourceResolverType.JCR_JACKRABBIT);
+
+
+    @Test
+    public void testSomething() {
+        Resource resource = context.resourceResolver().getResource("/content/sample/en");
+        // further testing
+    }
+
 }
